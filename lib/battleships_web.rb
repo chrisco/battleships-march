4b8f1e2ed2588f6dc1313b21e67dafccc5a3cb1b
Christopher Comella <git.chrisco@gmail.com>
2015-09-17 21:36:47 +0200

    Create boards for players and save to session. Also refactor some.

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -1,44 +1,39 @@
-require './lib/player.rb'
 require 'sinatra/base'
+require './lib/board.rb'
+require './lib/cell.rb'
+require './lib/player.rb'
+require './lib/ship.rb'
+require './lib/water.rb'
 require 'byebug'
-
-
 
 class BattleshipsWeb < Sinatra::Base
   set :views, proc {File.join(root, '..', 'views')}
   enable :sessions
 
   get '/' do
-    @name1 = params[:name1]
-    @name2 = params[:name2]
     erb :index
   end
 
   get '/new_game' do
-  @name1 = params[:name1]
-  @name2 = params[:name2]
-  erb :new_game
+    erb :new_game
   end
 
   post '/new_game' do
-    "Hello!"
     @name1 = params[:name1]
     @name2 = params[:name2]
     p1 = Player.new(@name1)
     p2 = Player.new(@name2)
+    p1.board = Board.new({size: 100, cell: Cell, number_of_pieces: 5})
+    p2.board = Board.new({size: 100, cell: Cell, number_of_pieces: 5})
     session[:name1] = params[:name1]
     session[:name2] = params[:name2]
     session[:p1] = p1
     session[:p2] = p2
+    session[:p1b] = p1.board
+    session[:p2b] = p2.board
     erb :new_game
   end
 
-  get '/settings' do
-    @player1_name = session[:name1]
-    @player2_name = session[:name2]
-    erb :settings
-  end
-
-  # start the server if ruby file executed directly
+  # Start server if Ruby file executed directly
   run! if app_file == $0
 end
